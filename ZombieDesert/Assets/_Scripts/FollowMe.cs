b3e35de261befde35d5fbe9971290b7abd0fbd0c
LeandroGuinan <69321106+LeandroGuinan@users.noreply.github.com>
2021-03-18 13:55:16 +0100

    ZombieDesertClip
    a clip made it with cinemachine

---
+++ ZombieDesert/Assets/_Scripts/FollowMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowMe : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 dest;
+    private NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        dest = _agent.destination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(dest, target.position) > 1.0f)
+        {
+            dest = target.position;
+            _agent.destination = dest;
+        }
+    }
+}
---
+++ ZombieDesert/Assets/_Scripts/MoveForward.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+
+    public float speed;
+    private GameObject _player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _player.transform.Translate(_player.transform.right*(speed*Time.deltaTime));
+        
+    }
+}
